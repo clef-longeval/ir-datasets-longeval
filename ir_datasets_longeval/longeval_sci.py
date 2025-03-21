5b070b704b82e7d20eeffa5051f7efbc200b7884
Jüri Keller <jueri.keller@th-koeln.de>
2025-03-21 13:03:34 +0100

    update path to abstract dataset

--- ir_datasets_longeval/longeval_sci.py
+++ ir_datasets_longeval/longeval_sci.py
@@ -1,15 +1,14 @@
+import contextlib
+import json
 import os
+from datetime import datetime
+from pathlib import Path
 from typing import Dict, List, NamedTuple, Optional
 
 from ir_datasets import registry
 from ir_datasets.datasets.base import Dataset
 from ir_datasets.formats import JsonlDocs, TrecQrels, TsvQueries
 from ir_datasets.util import ZipExtractCache, home_path
-from datetime import datetime
-
-import contextlib
-import json
-from pathlib import Path
 
 from ir_datasets_longeval.util import DownloadConfig, YamlDocumentation
 
@@ -55,22 +54,33 @@ class LongEvalSciDoc(NamedTuple):
     def default_text(self):
         return self.title + " " + self.abstract
 
-class ExtractedPath():
+
+class ExtractedPath:
     def __init__(self, path):
         self._path = path
 
     @contextlib.contextmanager
     def stream(self):
-        with open(self._path, 'rb') as f:
+        with open(self._path, "rb") as f:
             yield f
 
+
 class LongEvalSciDataset(Dataset):
-    def __init__(self, base_path: Path, yaml_documentation: str = "longeval_sci.yaml", timestamp: Optional[str] = None, prior_datasets: Optional[List[str]] = None):
+    def __init__(
+        self,
+        base_path: Path,
+        yaml_documentation: str = "longeval_sci.yaml",
+        timestamp: Optional[str] = None,
+        prior_datasets: Optional[List[str]] = None,
+    ):
         documentation = YamlDocumentation(yaml_documentation)
+        base_path = base_path / "longeval_sci_training_2025_abstract"
         self.base_path = base_path
 
         if not base_path or not base_path.exists() or not base_path.is_dir():
-            raise ValueError(f'I expected that the directory {base_path} exists. But the directory does not exist.')
+            raise ValueError(
+                f"I expected that the directory {base_path} exists. But the directory does not exist."
+            )
 
         if not timestamp:
             timestamp = self.read_property_from_metadata("timestamp")
@@ -79,40 +89,49 @@ class LongEvalSciDataset(Dataset):
 
         if prior_datasets is None:
             prior_datasets = self.read_property_from_metadata("prior-datasets")
-        
+
         self.prior_datasets = prior_datasets
 
-        docs_path = base_path / 'documents'
+        docs_path = base_path / "documents"
         if not docs_path.exists() or not docs_path.is_dir():
-            raise ValueError(f'I expected that the directory {docs_path} exists. But the directory does not exist.')
-        
+            raise ValueError(
+                f"I expected that the directory {docs_path} exists. But the directory does not exist."
+            )
+
         jsonl_doc_files = os.listdir(docs_path)
         if len(jsonl_doc_files) == 0:
-            raise ValueError(f'The directory {docs_path} has no jsonl files. This is likely an arror.')
+            raise ValueError(
+                f"The directory {docs_path} has no jsonl files. This is likely an arror."
+            )
 
         docs = JsonlDocs(
-            [ExtractedPath(base_path/ "documents" / split) for split in jsonl_doc_files],
+            [
+                ExtractedPath(base_path / "documents" / split)
+                for split in jsonl_doc_files
+            ],
             doc_cls=LongEvalSciDoc,
             docstore_path=f"{docs_path}/docstore.pklz4",
-            mapping=MAPPING
+            mapping=MAPPING,
         )
 
-        queries_path = base_path / 'queries.txt'
+        queries_path = base_path / "queries.txt"
         if not queries_path.exists() or not queries_path.is_file():
-            raise ValueError(f'I expected that the file {queries_path} exists. But the directory does not exist.')
+            raise ValueError(
+                f"I expected that the file {queries_path} exists. But the directory does not exist."
+            )
 
         queries = TsvQueries(ExtractedPath(queries_path))
         qrels = None
-        qrels_path = base_path / 'qrels.txt'
+        qrels_path = base_path / "qrels.txt"
 
         if qrels_path.exists() and qrels_path.is_file():
             qrels = TrecQrels(ExtractedPath(qrels_path), QREL_DEFS)
 
         super().__init__(docs, queries, qrels, documentation)
-    
+
     def get_timestamp(self):
         return self.timestamp
-    
+
     def get_past_datasets(self):
         return [LongEvalSciDataset(self.base_path / i) for i in self.prior_datasets]
 
@@ -121,7 +140,7 @@ class LongEvalSciDataset(Dataset):
 
 
 def register():
-    if f'{NAME}/2024-11/train' in registry:
+    if f"{NAME}/2024-11/train" in registry:
         # Already registered.
         return
 
@@ -136,7 +155,9 @@ def register():
 
     subsets = {}
 
-    subsets["2024-11/train"] = LongEvalSciDataset(data_path, "2024-11/train", "2024-11", [])
+    subsets["2024-11/train"] = LongEvalSciDataset(
+        data_path, "2024-11/train", "2024-11", []
+    )
 
     for s in sorted(subsets):
         registry.register(f"{NAME}/{s}", subsets[s])
