c37b21e00140bf6e043b884ccc82510244260d0a
Jüri Keller <jueri.keller@th-koeln.de>
2026-02-09 17:46:51 +0100

    start work on LongEval Sci 2026

---
+++ ir_datasets_longeval/datasets/longeval_sci_2026.py
@@ -0,0 +1,336 @@
+import contextlib
+import json
+import os
+from datetime import datetime
+from pathlib import Path
+from pkgutil import get_data
+from typing import Dict, List, NamedTuple, Optional
+
+from ir_datasets import registry
+from ir_datasets.datasets.base import Dataset
+from ir_datasets.formats import JsonlDocs, TrecQrels, TsvQueries
+from ir_datasets.util import ZipExtractCache, home_path
+
+from ir_datasets_longeval.formats import MetaDataset
+from ir_datasets_longeval.util import DownloadConfig, YamlDocumentation
+
+NAME = "longeval-sci-2026"
+QREL_DEFS = {
+    2: "highly relevant",
+    1: "relevant",
+    0: "not relevant",
+}
+SUB_COLLECTIONS = ["2024-11"]
+MAPPING = {
+    "doc_id": "id",
+    "title": "title",
+    "abstract": "abstract",
+    "authors": "authors",
+    "createdDate": "createdDate",
+    "doi": "doi",
+    "arxivId": "arxivId",
+    "pubmedId": "pubmedId",
+    "magId": "magId",
+    "oaiIds": "oaiIds",
+    "links": "links",
+    "publishedDate": "publishedDate",
+    "updatedDate": "updatedDate",
+}
+
+
+class LongEvalSciDoc(NamedTuple):
+    doc_id: str
+    title: str
+    abstract: str
+    authors: List[Dict[str, str]]
+    createdDate: Optional[str]
+    doi: Optional[str]
+    arxivId: Optional[str]
+    pubmedId: Optional[str]
+    magId: Optional[str]
+    oaiIds: Optional[List[str]]
+    links: List[Dict[str, str]]
+    publishedDate: str
+    updatedDate: str
+
+    def default_text(self):
+        ret = self.title
+        if self.abstract:
+            ret += " " + self.abstract
+        return ret
+
+
+class ExtractedPath:
+    def __init__(self, path):
+        self._path = path
+
+    def path(self, force=True):
+        if force and not self._path.exists():
+            raise FileNotFoundError(self._path)
+        return self._path
+
+    @contextlib.contextmanager
+    def stream(self):
+        with open(self._path, "rb") as f:
+            yield f
+
+
+def docs_iter_without_duplicates(iterator):
+    returned = set()
+    for doc in iterator:
+        if doc.doc_id not in returned and doc.default_text():
+            returned.add(doc.doc_id)
+            yield doc
+
+
+class LongEvalSciDataset(Dataset):
+    def __init__(
+        self,
+        base_path: Path,
+        yaml_documentation: str = "longeval_sci_2026.yaml",
+        timestamp: Optional[str] = None,
+        prior_datasets: Optional[List[str]] = None,
+        snapshot: Optional[str] = None,
+        qrels_path: Optional[Path] = None,
+    ):
+        """LongEvalSciDataset class
+
+        Args:
+            base_path (Path): Base path to the root of a typical longeval sci dataset directory structure.
+            yaml_documentation (str, optional): Documentation file. Defaults to "longeval_sci_2026.yaml".
+            timestamp (Optional[str], optional): Timestamp in the YYYY-MM format of the snapshot. Defaults to None.
+            prior_datasets (Optional[List[str]], optional): List of all snapshot that come before this snapshot. Defaults to None.
+            snapshot (Optional[str], optional): Name of the sub-collection. This was previously also referred to as lag and is most likely similar to the dataset_id. Defaults to None.
+        """
+        documentation = YamlDocumentation(yaml_documentation)
+        self.base_path = base_path
+
+        if not base_path or not base_path.exists() or not base_path.is_dir():
+            raise FileNotFoundError(
+                f"I expected that the directory {base_path} exists. But the directory does not exist."
+            )
+
+        if not timestamp:
+            timestamp = self.read_property_from_metadata("timestamp")
+
+        if timestamp is None:
+            raise ValueError("Timestamp cannot be None.")
+        self.timestamp = datetime.strptime(timestamp, "%Y-%m")
+
+        if not snapshot:
+            try:
+                snapshot = self.read_property_from_metadata("snapshot")
+            except KeyError:
+                snapshot = None
+        self.snapshot = snapshot
+
+        if prior_datasets is None:
+            prior_datasets = self.read_property_from_metadata("prior-datasets")
+
+        self.prior_datasets = prior_datasets
+
+        docs_path = base_path / snapshot.strip("-train") / "documents"
+
+        if not docs_path.exists() or not docs_path.is_dir():
+            raise FileNotFoundError(
+                f"I expected that the directory {docs_path} exists. But the directory does not exist."
+            )
+
+        jsonl_doc_files = os.listdir(docs_path)
+        if len(jsonl_doc_files) == 0:
+            raise ValueError(
+                f"The directory {docs_path} has no jsonl files. This is likely an error."
+            )
+        docs_paths = []
+        for file in jsonl_doc_files:
+            if file.endswith(".jsonl"):
+                docs_paths.append(ExtractedPath(docs_path / file))
+        docs = JsonlDocs(
+            docs_paths,
+            doc_cls=LongEvalSciDoc,
+            docstore_path=f"{docs_path}/docstore.pklz4",
+            mapping=MAPPING,
+        )
+
+        if "train" in snapshot:
+            queries = TsvQueries(ExtractedPath(base_path / "queries-train.tsv"))
+        else:
+            queries = TsvQueries(ExtractedPath(base_path / "queries-test.tsv"))
+
+        qrels = None
+        if qrels_path is not None and qrels_path.exists() and qrels_path.is_file():
+            qrels = TrecQrels(ExtractedPath(qrels_path), QREL_DEFS)
+
+        super().__init__(docs, queries, qrels, documentation)
+        original_iter = self.docs_iter
+        self.docs_iter = lambda: docs_iter_without_duplicates(original_iter())
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_snapshot(self):
+        return self.snapshot
+
+    def get_datasets(self):
+        return None
+
+    def get_prior_datasets(self):
+        if not self.prior_datasets:
+            return []
+        elif isinstance(self.prior_datasets[0], str):
+            # prior_datasets = []
+            # for snapshot in self.prior_datasets:
+            #     prior_datasets.append(
+            #         LongEvalSciDataset(
+            #             base_path=self.base_path ,
+            #             prior_datasets=self.prior_datasets[:-1],
+            #             snapshot=self.snapshot[:4],
+            #             timestamp="2025-" + self.snapshot[:2],
+            #         )
+            #     )
+            return [
+                LongEvalSciDataset(self.base_path, snapshot=i)
+                for i in self.prior_datasets
+            ]
+        else:
+            return self.prior_datasets
+
+    def read_property_from_metadata(self, property):
+        try:
+            return json.load(open(self.base_path / "etc" / "metadata.json", "r"))[
+                property
+            ]
+        except FileNotFoundError:
+            metadata = json.loads(get_data("ir_datasets_longeval", "etc/metadata.json"))
+            return metadata[f"longeval-sci/{self.timestamp.strftime('%Y-%m')}/train"][
+                property
+            ]
+
+
+def register():
+    if f"{NAME}/03-05" in registry:
+        # Already registered.
+        return
+    base_path = home_path() / NAME
+    dlc = DownloadConfig.context(NAME, base_path)
+
+    subsets = {}
+
+    # 2026 train
+    data_path = ZipExtractCache(dlc["longeval_sci_2026"], base_path).path()
+
+    subsets["03-05-train"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05-train",
+    )
+    subsets["03-05-train/raw"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05-train",
+        qrels_path=data_path / "03-05" / "qrels-snapshot-train-raw.txt",
+    )
+    subsets["03-05-train/dctr"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05-train",
+        qrels_path=data_path / "03-05" / "qrels-snapshot-train-dctr.txt",
+    )
+
+    # 2026 test
+
+    ### 03-05
+    subsets["03-05"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05",
+    )
+    subsets["03-05/raw"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05",
+        qrels_path=data_path / "03-05" / "qrels-snapshot-1-raw.txt",
+    )
+    subsets["03-05/dctr"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-03",
+        prior_datasets=[],
+        snapshot="03-05",
+        qrels_path=data_path / "03-05" / "qrels-snapshot-1-dctr.txt",
+    )
+
+    ### 06-08
+    subsets["06-08"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-06",
+        prior_datasets=[subsets["03-05"]],
+        snapshot="06-08",
+    )
+    subsets["06-08/raw"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-06",
+        prior_datasets=[subsets["03-05/raw"]],
+        snapshot="06-08",
+        qrels_path=data_path / "06-08" / "qrels-snapshot-2-raw.txt",
+    )
+    subsets["06-08/dctr"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-06",
+        prior_datasets=[subsets["03-05/dctr"]],
+        snapshot="06-08",
+        qrels_path=data_path / "06-08" / "qrels-snapshot-2-dctr.txt",
+    )
+
+    ### 09-11
+    subsets["09-11"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-09",
+        prior_datasets=[subsets["06-08/raw"], subsets["03-05/raw"]],
+        snapshot="09-11",
+    )
+    subsets["09-11/raw"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-09",
+        prior_datasets=[subsets["06-08/raw"], subsets["03-05/raw"]],
+        snapshot="09-11",
+        qrels_path=data_path / "09-11" / "qrels-snapshot-3-raw.txt",
+    )
+    subsets["09-11/dctr"] = LongEvalSciDataset(
+        base_path=data_path,
+        timestamp="2025-09",
+        prior_datasets=[subsets["06-08/dctr"], subsets["03-05/dctr"]],
+        snapshot="09-11",
+        qrels_path=data_path / "09-11" / "qrels-snapshot-3-dctr.txt",
+    )
+
+    for s in sorted(subsets):
+        registry.register(f"{NAME}/{s}", subsets[s])
+
+    if f"{NAME}/*" in registry:
+        return
+    registry.register(
+        f"{NAME}/*", MetaDataset([subsets["03-05"], subsets["06-08"], subsets["09-11"]])
+    )
+
+    ### Meta datasets
+    if f"{NAME}/clef-2026-test" in registry:
+        return
+    registry.register(
+        f"{NAME}/clef-2026-test",
+        MetaDataset([subsets["03-05"], subsets["06-08"], subsets["09-11"]]),
+    )
+    registry.register(
+        f"{NAME}/clef-2026-test/raw",
+        MetaDataset([subsets["03-05/raw"], subsets["06-08/raw"], subsets["09-11/raw"]]),
+    )
+    registry.register(
+        f"{NAME}/clef-2026-test/dctr",
+        MetaDataset(
+            [subsets["03-05/dctr"], subsets["06-08/dctr"], subsets["09-11/dctr"]]
+        ),
+    )
---
+++ tests/test_longeval_sci_2026.py
@@ -0,0 +1,229 @@
+from datetime import datetime
+
+import pytest
+
+from ir_datasets_longeval import load
+
+UNJUDGED_QUERIES = {
+    "03-05-train/raw": {},
+    "03-05-train/dctr": {"a17857d1a641623051cc8b231abd516b"},
+}
+
+
+class TestLongEval2023Snapshot:
+    @pytest.fixture(
+        scope="class",
+        params=[
+            # train
+            {
+                "snapshot": "03-05-train",
+                "n_queries": 100,
+                "expected_queries": {"e54f68f74633d43b86d0247af6197544": "mark twain"},
+                "n_qrels": 0,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            {
+                "snapshot": "03-05-train/raw",
+                "n_queries": 100,
+                "expected_queries": {"e54f68f74633d43b86d0247af6197544": "mark twain"},
+                "n_qrels": 5253,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            {
+                "snapshot": "03-05-train/dctr",
+                "n_queries": 100,
+                "expected_queries": {"e54f68f74633d43b86d0247af6197544": "mark twain"},
+                "n_qrels": 14960,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            ## 03-05
+            {
+                "snapshot": "03-05",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 0,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            {
+                "snapshot": "03-05/raw",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 10045,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            {
+                "snapshot": "03-05/dctr",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 19366,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": [],
+            },
+            ## 06-09
+            {
+                "snapshot": "06-08",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 0,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["03-05"],
+            },
+            {
+                "snapshot": "06-08/raw",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 27207,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["03-05"],
+            },
+            {
+                "snapshot": "06-08/dctr",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 17563,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["03-05"],
+            },
+            ## 09-11
+            {
+                "snapshot": "09-11",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 0,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["06-08", "03-05"],
+            },
+            {
+                "snapshot": "09-11/raw",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 104498,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["06-08", "03-05"],
+            },
+            {
+                "snapshot": "09-11/dctr",
+                "n_queries": 315,
+                "expected_queries": {
+                    "a2a64265721427605ec182827171265b": "cybersecurity"
+                },
+                "n_qrels": 9374,
+                "expected_docs": {"40693434": "An Example of a Mansion in Tokat"},
+                "prior_snapshots": ["06-08", "03-05"],
+            },
+        ],
+    )
+    def snapshot_data(self, request):
+        snapshot = request.param["snapshot"]
+
+        loaded_datasets = load(f"longeval-sci-2026/{snapshot}")
+        yield loaded_datasets, request.param
+
+    def test_snapshot_exists(self, snapshot_data):
+        dataset, setting = snapshot_data
+        assert dataset is not None
+        assert dataset.get_snapshot() == setting["snapshot"].split("/")[0]
+
+    def test_queries(self, snapshot_data):
+        dataset, setting = snapshot_data
+
+        assert dataset.has_queries()
+
+        actual_queries = {i.query_id: i.default_text() for i in dataset.queries_iter()}
+        assert len(actual_queries) == setting["n_queries"]
+        for k, v in setting["expected_queries"].items():
+            assert v == actual_queries[k]
+
+    def test_docs(self, snapshot_data):
+        dataset, setting = snapshot_data
+
+        assert dataset.has_docs()
+
+        example_doc = dataset.docs_iter().__next__()
+        assert example_doc is not None
+
+    def test_docstore(self, snapshot_data):
+        dataset, setting = snapshot_data
+
+        docs_store = dataset.docs_store()
+
+        for docid, title in setting["expected_docs"].items():
+            assert docs_store.get(docid).doc_id == docid
+            assert docs_store.get(docid).title == title
+
+    def test_qrels(self, snapshot_data):
+        dataset, setting = snapshot_data
+
+        if setting["n_qrels"] == 0:
+            assert not dataset.has_qrels()
+            return
+        assert dataset.has_qrels()
+
+        qrels = list(dataset.qrels_iter())
+        assert len(qrels) == setting["n_qrels"]
+
+        # test qrels
+        # all queries have judgments
+        # all qids in qrels are in queries
+        # qids_in_qrels = {qrel.query_id for qrel in dataset.qrels_iter()}
+        # qids_in_qrels = qids_in_qrels.union(
+        #     UNJUDGED_QUERIES.get(setting["snapshot"], set())
+        # )
+        # qids_in_queries = {query.query_id for query in dataset.queries_iter()}
+        # assert qids_in_qrels == qids_in_queries
+
+        # # all qids have relevant docs
+        # qids_in_qrels = {
+        #     qrel.query_id for qrel in dataset.qrels_iter() if qrel.relevance > 0
+        # }
+        # qids_in_qrels = qids_in_qrels.union(
+        #     UNJUDGED_QUERIES.get(setting["snapshot"], set())
+        # )
+
+        # assert qids_in_qrels == qids_in_queries
+
+        ## TODO: Check with corpus
+        # # all docids in qrels are in docs
+        # docs_store = dataset.docs_store()
+        # docids_in_qrels = {qrel.doc_id for qrel in dataset.qrels_iter()}
+        # for doc_id in docids_in_qrels:
+        #     assert docs_store.get(doc_id) is not None
+
+    def test_timestamp(self, snapshot_data):
+        dataset, setting = snapshot_data
+        assert dataset.get_timestamp() == datetime.strptime(
+            ("2025-" + setting["snapshot"])[:7], "%Y-%m"
+        )  # ignore "-test"
+
+    def test_prior_datasets(self, snapshot_data):
+        dataset, setting = snapshot_data
+
+        prior_datasets = dataset.get_prior_datasets()
+        assert len(prior_datasets) == len(setting["prior_snapshots"])
+
+        for i, prior_snapshot in enumerate(prior_datasets):
+            assert prior_snapshot.get_snapshot() == setting["prior_snapshots"][i]
+            assert len(prior_snapshot.get_prior_datasets()) == len(
+                setting["prior_snapshots"][i + 1 :]
+            )
